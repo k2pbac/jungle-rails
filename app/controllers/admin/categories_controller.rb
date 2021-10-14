6a8b0c9b1eae63e1c47c079bb2c8051d9f28e89c
k2pbac <krisbachan123@hotmail.com>
2021-10-14 16:56:48 -0400

    add route for create and new category, add ui for creating

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,30 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.joins("INNER JOIN products ON products.category_id = categories.id")
+    @categories = Category.joins("LEFT JOIN products ON products.category_id = categories.id")
     .select('categories.name, Count(products.*)').group('categories.id')
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+
 end
