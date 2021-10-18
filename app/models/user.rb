5321c76f92d9663c8bc7fe0593d653d3952ba277
k2pbac <krisbachan123@hotmail.com>
2021-10-18 19:46:40 -0400

    update user model with validation for password, name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates_uniqueness_of :email
+  validate :email, presence: true
+  validate :name, presence: true
+  validate :password_and_confirmation
+
+  def password_and_confirmation
+    errors.add(:password, "passwords don't match") if password != password_confirmation
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid when password and password_confirmation are the same' do
+      user = User.new('person', 'person@persons.com', 'password', 'password')
+      def params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+      expect(user).to be_valid
+    end
+  end
+
 end
